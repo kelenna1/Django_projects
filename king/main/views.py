04fca282c402544acae8d1e24adc147486d551ff
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-11-24 19:39:35 +0100

    updating

--- king/main/views.py
+++ king/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
 def index(response, id):
@@ -9,3 +10,7 @@ def index(response, id):
 
 def home(response):
     return render (response, "main/home.html", {})
+
+def create(response):
+    form = CreateNewList
+    return render (response, "main/create.html", {"form": form})
